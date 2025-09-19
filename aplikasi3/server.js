748896d297d2ad95536e1b613a77abb5e5a43206
nurbelawahyuningse2327240105-ux <nurbelawahyuningse_2327240105@mhs.mdp.ac.id>
2025-09-19 17:12:06 +0700

    pertemuan 3
    latihan my express

---
+++ aplikasi3/server.js
@@ -0,0 +1,10 @@
+const express = requiere ("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'))
+
+app.listen(port, (req, res)=>{
+    console.log('Server Running at http://localhost')
+
+});
